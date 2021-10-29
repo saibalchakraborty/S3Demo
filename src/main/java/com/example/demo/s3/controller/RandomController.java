b9c0de542921d2468bf37f0cb20b238104edd850
saibalchakraborty <saibal183@gmail.com>
2021-10-29 20:16:21 +0530

    upload file functionality

--- src/main/java/com/example/demo/s3/controller/RandomController.java
+++ src/main/java/com/example/demo/s3/controller/RandomController.java
@@ -6,8 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.example.demo.s3.api.model.ResponseMsg;
 import com.example.demo.s3.service.RandomService;
 import lombok.extern.slf4j.Slf4j;
@@ -36,6 +40,18 @@ public class RandomController {
 		} catch (Exception e) {
 			log.error("Failed generating presigned url {}", e.getLocalizedMessage());
 			return new ResponseEntity<ResponseMsg>(ResponseMsg.builder().error(e.getMessage()).build(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@PostMapping(value = "/uploadDocument")
+	public String uploadDoocument(@RequestParam MultipartFile file) {
+		try {
+			String fileName = service.uploadFile(file);
+			return fileName;
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error("Failed uploading {}", e.getLocalizedMessage());
+			return "Failed!";
 		}
 	}
 }
--- src/main/java/com/example/demo/s3/service/RandomService.java
+++ src/main/java/com/example/demo/s3/service/RandomService.java
@@ -1,13 +1,23 @@
 package com.example.demo.s3.service;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Service
@@ -33,4 +43,22 @@ public class RandomService {
 		URL url = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
 		return url;
 	}
+
+
+	public String uploadFile(MultipartFile file) throws Exception {
+		String objectKey = file.getOriginalFilename();
+		File convert = convert(file);
+		TransferManager transferManager = TransferManagerBuilder.standard().withS3Client(s3Client).build();
+		transferManager.upload(bucketName, objectKey, convert);
+		return objectKey;
+	}
+	
+	private File convert(MultipartFile file) throws IOException {
+	    File convFile = new File(file.getOriginalFilename());
+	    convFile.createNewFile();
+	      try(FileOutputStream fos = new FileOutputStream(convFile)) {
+	    	  fos.write(file.getBytes());
+	      }
+	    return convFile;
+	  }
 }
